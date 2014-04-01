4a2d56af97e27ff9e61595445c2d659dd37c511a
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-04-01 20:13:19 -0300

    Arquivo modificado para conexão com Banco de Dados

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,8 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+   
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -14,30 +14,26 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://localhost/sec-treasurer2?useUnicode=yes&characterEncoding=UTF-8"
+	    username = "root"
+            password = "mathandfis"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://localhost/sec-treasurer-prod2?useUnicode=yes&characterEncoding=UTF-8"
+            username = "root"
+            password = "mathandfis"
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
+            url = "jdbc:mysql://localhost/sec-treasurer-prod2?useUnicode=yes&characterEncoding=UTF-8"
+            username = "root"
+            password = "mathandfis"
+            
         }
     }
 }
